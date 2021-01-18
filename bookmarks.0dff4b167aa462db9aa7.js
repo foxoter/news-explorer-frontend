9f2f382318636e6653298d9fdd78a72b155d83da
bulat kt <bulat.kt@yandex.ru>
2021-01-18 13:38:29 +0300

    Updates

---
+++ bookmarks.0dff4b167aa462db9aa7.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e=document.querySelector(".header__button"),t=(document.querySelector(".header"),document.querySelector(".header__navigation"));e.addEventListener("click",(function(){t.classList.toggle("header__navigation_state-shown"),e.classList.toggle("header__button_type-cross-black")}))})();
